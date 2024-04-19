04763e5476a6294bc657d3a9207d233cfe495d4d
Brian <brian.stith0331@gmail.com>
2024-04-18 20:29:22 -0400

    array of tempoarary books has been created and imported new java.util

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,13 +1,40 @@
 package com.ps;
 import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.util.Map;
+import java.util.HashMap;
 public class Main {
 
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-
         int userInput;
+
+        Book[] library = new Book[21];
+        library [0] = new Book(0, "1111-1111-1111", "Book 1",  false, "None");
+        library [1] = new Book(0, "1111-1111-1112", "Book 2",  false, "None");
+        library [2] = new Book(0, "1111-1111-1113", "Book 3",  false, "None");
+        library [3] = new Book(0, "1111-1111-1114", "Book 4",  false, "None");
+        library [4] = new Book(0, "1111-1111-1115", "Book 5",  false, "None");
+        library [5] = new Book(0, "1111-1111-1116", "Book 6",  false, "None");
+        library [6] = new Book(0, "1111-1111-1117", "Book 7",  false, "None");
+        library [7] = new Book(0, "1111-1111-1118", "Book 8",  false, "None");
+        library [8] = new Book(0, "1111-1111-1119", "Book 9",  false, "None");
+        library [9] = new Book(0, "1111-1111-1120", "Book 10",  false, "None");
+        library [10] = new Book(0, "1111-1111-1121", "Book 11",  false, "None");
+        library [11] = new Book(0, "1111-1111-1122", "Book 12",  false, "None");
+        library [12] = new Book(0, "1111-1111-1123", "Book 13",  false, "None");
+        library [13] = new Book(0, "1111-1111-1124", "Book 14",  false, "None");
+        library [14] = new Book(0, "1111-1111-1125", "Book 15",  false, "None");
+        library [15] = new Book(0, "1111-1111-1126", "Book 16",  false, "None");
+        library [16] = new Book(0, "1111-1111-1127", "Book 17",  false, "None");
+        library [17] = new Book(0, "1111-1111-1128", "Book 18",  false, "None");
+        library [18] = new Book(0, "1111-1111-1129", "Book 19",  false, "None");
+        library [19] = new Book(0, "1111-1111-1130", "Book 20",  false, "None");
+        library [20] = new Book(0, "1111-1111-1131", "Book 21",  false, "None");
+
+        Map <Integer,String> availableBooks;
 
 //        do{
 
